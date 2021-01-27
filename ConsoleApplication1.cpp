58cee65c0749e57a517cd20c53f2798fe2c3c2de
nsushant <64201587+nsushant@users.noreply.github.com>
2021-01-27 19:13:27 +0000

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -6,31 +6,19 @@
 #include<cmath> 
 
 int main()
-{
+{	//Count keeps track of the number of words entered
 	int count = 0;
+	//initialize variables
 	std::string previousword = " ";
 	std::string word;
+	//keep accepting words until terminated 
 	while (std::cin >> word) {
 		++count;
 		if (previous == word)
+			//if a word is repeated, tell me where it is and what it is
 			std::cout << "word number " << count
 			<< " repeated: " << word << '\n';
 		previous = word;
 	}
 }
 
-
-
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
